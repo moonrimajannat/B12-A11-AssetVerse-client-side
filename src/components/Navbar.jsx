469aafc66ca9b6fba06a26896e80dc6ba0f376d4
moonrimajannat <jannatmoon573@gmail.com>
2025-12-25 17:40:46 +0600

    fixed the bug

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -140,8 +140,8 @@ const Navbar = () => {
                     <div className="dropdown dropdown-end">
                         <div tabIndex={0} role="button" className="btn bg-blue-600 text-white m-1">Register</div>
                         <ul tabIndex="-1" className="dropdown-content menu bg-blue-100 rounded-box z-1 w-52 p-2 shadow-sm">
-                            <NavLink to="/hr-register"><li><Link>HR</Link></li></NavLink>
-                            <NavLink to="/employee-register"><li><Link>Employee</Link></li></NavLink>
+                            <Link to="/hr-register"><li className="mb-2">HR</li></Link>
+                            <Link to="/employee-register"><li>Employee</li></Link>
                         </ul>
                     </div>
                 </div>
